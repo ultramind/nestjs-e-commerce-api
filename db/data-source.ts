a6be5e6c17bce214fbca553530badb3caa7ed271
Akachukwu <udechukwuakachukwu@gmail.com>
2025-02-22 03:03:24 +0100

    Created UserEntity

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,9 +10,9 @@ export const dataSourceOptions:DataSourceOptions = {
     username:process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
-    entities: [],
+    entities: ['dist/**/*.entity{.ts,.js}'],
     migrations:[],
-    synchronize:false,
+    synchronize:true,
     logging:false
 }
 
