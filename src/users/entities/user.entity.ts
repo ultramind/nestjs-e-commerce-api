b72fd672aa19fdcd4754ba67f14498b3772566dd
Akachukwu <udechukwuakachukwu@gmail.com>
2025-02-22 03:18:14 +0100

    Created the Users Resource and Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Roles } from "utils/commons/user-roles.enum";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+
+    @Column({type:'enum', enum: Roles, default: [Roles.USER]})
+    roles: Roles[];
+}
